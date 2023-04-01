7b330046ce35650690dca01417967b4e1214dba9
SM Sohag <sohagsheik32@gmail.com>
2023-04-01 23:29:48 +0600

    layout stracture changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Hero from "./components/Navbar/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="bg-black text-white">
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, voluptatum
+      consequuntur. Porro veniam natus eaque possimus ratione, excepturi est qui
+      atque error quis nesciunt libero quo tempora debitis facere voluptatum.
+    </div>
+  );
+};
+
+export default Hero;
