f6f31a7c33dc640eccfe5de76e40a9ad51fe1377
SM Sohag <sohagsheik32@gmail.com>
2023-04-02 20:00:35 +0600

    phone  chart add

--- src/components/PhoneBar/PhoneBar.jsx
+++ src/components/PhoneBar/PhoneBar.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const PhoneBar = () => {
-  return <div></div>;
+  const [phones, setPhones] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+      .then((data) => {
+        const loadedData = data.data.data;
+        console.log(loadedData);
+        const phonesData = loadedData.map((phone) => {
+          const parts = phone.slug.split("-");
+          const price = parseInt(parts[1]);
+          const phoneInfo = {
+            name: phone.phone_name,
+            price: price,
+          };
+          return phoneInfo;
+        });
+        setPhones(phonesData);
+      });
+  }, []);
+
+  return (
+    <div>
+      <BarChart width={800} height={300} data={phones}>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip />
+      </BarChart>
+    </div>
+  );
 };
 
 export default PhoneBar;
